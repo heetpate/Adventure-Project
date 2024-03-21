5a32519bdc37afc685acf0de6cbb1ce9ac04347a
heetp <heetp@HEET>
2024-03-20 22:27:57 -0400

    Add project files.

---
+++ Adventure Project/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Adventure_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.optionButton1 = new System.Windows.Forms.Button();
+            this.optionButton3 = new System.Windows.Forms.Button();
+            this.optionButton2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // optionButton1
+            // 
+            this.optionButton1.Location = new System.Drawing.Point(363, 197);
+            this.optionButton1.Name = "optionButton1";
+            this.optionButton1.Size = new System.Drawing.Size(75, 23);
+            this.optionButton1.TabIndex = 1;
+            this.optionButton1.Text = "option 1";
+            this.optionButton1.UseVisualStyleBackColor = true;
+            this.optionButton1.Click += new System.EventHandler(this.optionButton1_Click);
+            // 
+            // optionButton3
+            // 
+            this.optionButton3.Location = new System.Drawing.Point(363, 294);
+            this.optionButton3.Name = "optionButton3";
+            this.optionButton3.Size = new System.Drawing.Size(75, 23);
+            this.optionButton3.TabIndex = 2;
+            this.optionButton3.Text = "option 3";
+            this.optionButton3.UseVisualStyleBackColor = true;
+            // 
+            // optionButton2
+            // 
+            this.optionButton2.Location = new System.Drawing.Point(363, 243);
+            this.optionButton2.Name = "optionButton2";
+            this.optionButton2.Size = new System.Drawing.Size(75, 23);
+            this.optionButton2.TabIndex = 3;
+            this.optionButton2.Text = "option 2";
+            this.optionButton2.UseVisualStyleBackColor = true;
+            this.optionButton2.Click += new System.EventHandler(this.optionButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.optionButton2);
+            this.Controls.Add(this.optionButton3);
+            this.Controls.Add(this.optionButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button optionButton1;
+        private System.Windows.Forms.Button optionButton3;
+        private System.Windows.Forms.Button optionButton2;
+    }
+}
+
---
+++ Adventure Project/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Adventure_Project
+{
+    public partial class Form1 : Form
+    {
+        // Tracks the page of the story the user is on
+        int page =1;
+
+        //Random number generator
+        Random randGen = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void optionButton1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 11;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                int randValue = randGen.Next(1, 41);
+
+                if (randValue < 40)
+                {
+                    page = 6;
+                }
+                else
+                {
+                    page = 7;
+                }
+            }
+            else if (page == 5)
+            {
+                int randValue = randGen.Next(1, 41);
+
+                if (randValue < 40)
+                {
+                    page = 6;
+                }
+                else
+                {
+                    page = 7;
+                }
+            }
+            else if (page == 6)
+            {
+                page = 99;
+            }
+            else if (page == 7)
+            {
+                page = 10; //watch out for this one look at this option in the flowchart
+            }
+            else if (page == 8)
+            {
+                page = 10;
+            }
+            else if (page == 9)
+            {
+                page = 10;
+            }
+            else if (page == 10)
+            {
+                page = 11;
+            }
+
+            else if (page == 11)
+            {
+                page = 99;
+            }
+
+            else if (page == 12)
+            {
+                page = 13;
+            }
+            else if (page == 13)
+            {
+                page = 15;
+            }
+            else if (page == 14)
+            {
+                page = 99;
+            }
+            else if (page == 15)
+            {
+                page = 99;
+            }
+            else if (page == 16)
+            {
+                page = 17;
+            }
+            else if (page == 17)
+            {
+                page = 99;
+            }
+            else if (page == 18)
+            {
+                page = 19;
+            }
+            else if (page == 19)
+            {
+                page = 99;
+            }
+            else if (page == 20)
+            {
+                page = 21;
+            }
+            else if (page == 21)
+            {
+                page = 99;
+            }
+            else if (page == 22)
+            {
+                page = 23;
+            }
+            else if (page == 23)
+            {
+                page = 99;
+            }
+            else if (page == 24)
+            {
+                page = 99;
+            }
+            else if (page == 25)
+            {
+                page = 99;
+            }
+        }
+
+        private void optionButton2_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 12;
+            }
+            else if (page == 3)
+            {
+                page = 5;
+            }
+            else if (page == 4)
+            {
+                page = 8;
+            }
+            else if (page == 5)
+            {
+                page = 8;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                page = 9;
+            }
+            else if (page == 8)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                page = 10;
+            }
+            else if (page == 10)
+            {
+                page = 12;
+            }
+
+            else if (page == 11)
+            {
+                page = 1;
+            }
+
+            else if (page == 12)
+            {
+                page = 14;
+            }
+            else if (page == 13)
+            {
+                page = 15;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+            else if (page == 15)
+            {
+                page = 1;
+            }
+            else if (page == 16)
+            {
+                page = 18;
+            }
+            else if (page == 17)
+            {
+                page = 1;
+            }
+            else if (page == 18)
+            {
+                page = 20;
+            }
+            else if (page == 19)
+            {
+                page = 1;
+            }
+            else if (page == 20)
+            {
+                page = 22;
+            }
+            else if (page == 21)
+            {
+                page = 1;
+            }
+            else if (page == 22)
+            {
+                page = 24;
+            }
+            else if (page == 23)
+            {
+                page = 1;
+            }
+            else if (page == 24)
+            {
+                page = 1;
+            }
+            else if (page == 25)
+            {
+                page = 1;
+            }
+        }
+    }
+}
