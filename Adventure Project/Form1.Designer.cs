b44d8ecedf8d844f8469c6daf6283ed506fa121c
heetp <heetp@HEET>
2024-03-22 19:01:41 -0400

    The code done from home

--- Adventure Project/Form1.Designer.cs
+++ Adventure Project/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.optionButton1 = new System.Windows.Forms.Button();
             this.optionButton3 = new System.Windows.Forms.Button();
             this.optionButton2 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // optionButton1
             // 
-            this.optionButton1.Location = new System.Drawing.Point(363, 197);
+            this.optionButton1.Location = new System.Drawing.Point(3, 280);
             this.optionButton1.Name = "optionButton1";
-            this.optionButton1.Size = new System.Drawing.Size(75, 23);
+            this.optionButton1.Size = new System.Drawing.Size(196, 57);
             this.optionButton1.TabIndex = 1;
             this.optionButton1.Text = "option 1";
             this.optionButton1.UseVisualStyleBackColor = true;
@@ -45,28 +47,50 @@
             // 
             // optionButton3
             // 
-            this.optionButton3.Location = new System.Drawing.Point(363, 294);
+            this.optionButton3.Location = new System.Drawing.Point(528, 319);
             this.optionButton3.Name = "optionButton3";
-            this.optionButton3.Size = new System.Drawing.Size(75, 23);
+            this.optionButton3.Size = new System.Drawing.Size(196, 57);
             this.optionButton3.TabIndex = 2;
             this.optionButton3.Text = "option 3";
             this.optionButton3.UseVisualStyleBackColor = true;
+            this.optionButton3.Click += new System.EventHandler(this.optionButton3_Click);
             // 
             // optionButton2
             // 
-            this.optionButton2.Location = new System.Drawing.Point(363, 243);
+            this.optionButton2.Location = new System.Drawing.Point(264, 325);
             this.optionButton2.Name = "optionButton2";
-            this.optionButton2.Size = new System.Drawing.Size(75, 23);
+            this.optionButton2.Size = new System.Drawing.Size(196, 45);
             this.optionButton2.TabIndex = 3;
             this.optionButton2.Text = "option 2";
             this.optionButton2.UseVisualStyleBackColor = true;
             this.optionButton2.Click += new System.EventHandler(this.optionButton2_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(581, 112);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(51, 20);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "label2";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(556, 183);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(133, 76);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.optionButton2);
             this.Controls.Add(this.optionButton3);
             this.Controls.Add(this.optionButton1);
@@ -74,6 +98,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +107,8 @@
         private System.Windows.Forms.Button optionButton1;
         private System.Windows.Forms.Button optionButton3;
         private System.Windows.Forms.Button optionButton2;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- Adventure Project/Form1.cs
+++ Adventure Project/Form1.cs
@@ -9,19 +9,24 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using System.Media;
+using System.Drawing.Text;
 
 namespace Adventure_Project
 {
     public partial class Form1 : Form
     {
         // Tracks the page of the story the user is on
-        int page =1;
+        int page = 1;
 
         //Random number generator
         Random randGen = new Random();
         public Form1()
         {
             InitializeComponent();
+
+            optionButton1.Enabled = false;
+            optionButton2.Enabled = false;
+            optionButton3.Enabled = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -151,6 +156,8 @@ namespace Adventure_Project
             {
                 page = 99;
             }
+
+            DisplayPage();
         }
 
         private void optionButton2_Click(object sender, EventArgs e)
@@ -189,7 +196,7 @@ namespace Adventure_Project
             }
             else if (page == 9)
             {
-                page = 10;
+                page = 10; //correct this statement
             }
             else if (page == 10)
             {
@@ -207,7 +214,7 @@ namespace Adventure_Project
             }
             else if (page == 13)
             {
-                page = 15;
+                page = 16; //correct this statement
             }
             else if (page == 14)
             {
@@ -257,6 +264,209 @@ namespace Adventure_Project
             {
                 page = 1;
             }
+
+            DisplayPage();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            optionButton1.Show();
+            optionButton2.Show();
+
+            optionButton3.Hide();
+            startButton.Hide();
+            //pictureBox1.Hide();
+            //pictureBox2.Hide();
+
+            optionButton1.Enabled = true;
+            optionButton2.Enabled = true;
+            optionButton3.Enabled = true;
+
+            page = 1;
+
+            DisplayPage();
+        }
+        private void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You are in a campus with you school friends they are planning to go outside at night for camp fire. Go outside or stay?";
+                    optionButton1.Text = "Outside";
+                    optionButton2.Text = "Stay";
+                    break;
+                case 2:
+                    outputLabel.Text = "You light up the fire and having fun. ";
+                    Refresh();
+                    Thread.Sleep(2050);
+                    outputLabel.Text = "Your friend ran to you and tells you everything that happened";
+                    optionButton1.Text = "Run right away ";
+                    optionButton2.Text = "Find a safe place and make a plan";
+                    Refresh();
+                    break;
+                case 3:
+                    outputLabel.Text = "You stayed with your friend and suddenly you hear strange sounds from the basement and your teacher is out with the other friends, What would you do?";
+                    optionButton1.Text = "No";
+                    optionButton2.Text = "Explore";
+                    break;
+                case 4:
+                    outputLabel.Text = "You and your friend started a movie but the noise is to irritating so you decide to go, check anyways";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    outputLabel.Text = "You open the basement, it's pitch black and suddenly a strange looking creature jumped on your friend";
+                    optionButton1.Text = "Save";
+                    optionButton2.Text = "Get out of there";
+                    break;
+                case 5:
+                    outputLabel.Text = "You open the basement, it's pitch black and suddenly a strange looking creature jumped on you friend";
+                    optionButton1.Text = "Save";
+                    optionButton2.Text = "Get out of there";
+                    break;
+                case 6:
+                    outputLabel.Text = "You die saving your friend\r\nYour a TRUE FRIEND! \r\n Play Again";
+                    optionButton1.Text = "Yes ";
+                    optionButton2.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "You lived...\r\nYou locked the basement.";
+                    optionButton1.Text = "Call";
+                    optionButton2.Text = "Run";
+                    break;
+                case 8:
+                    outputLabel.Text = "You locked the basement";
+                    Refresh();
+                    Thread.Sleep(1500);
+                    outputLabel.Text = "Are you going to call your friends or run to your friends?";
+                    optionButton1.Text = "Run";
+                    optionButton2.Text = "Call";
+                    break;
+                case 9:
+                    outputLabel.Text = "The electricity ran out the last option is to run to them";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    page = 10;
+
+                    break;
+                case 10:
+                    outputLabel.Text = "You guys ran to your friends and told them everything";
+                    optionButton1.Text = "Run right away";
+                    optionButton2.Text = "Find a safe place and make a plan";
+                    break;
+                case 11:
+                    outputLabel.Text = "You and your friends got chased and killed by scary looking creature. \r\n Play Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 12:
+                    outputLabel.Text = "You find a house where no one lives there but to keep yourself alive you decide to break into it";
+                    optionButton1.Text = "Explore";
+                    optionButton2.Text = "No";
+                    break;
+                case 13:
+                    outputLabel.Text = "Your safe! \r\nYou also find a gun, some food and water for temporary, the map to escape.";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    outputLabel.Text = "Your plan is to wait for the electricity to call or go outside and escape yourself";
+                    optionButton1.Text = "Wait";
+                    optionButton2.Text = "Try escaping";
+                    break;
+                case 14:
+                    outputLabel.Text = "The next day you were killed by the creature\r\nPlay Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 15:
+                    outputLabel.Text = "You have waited to long, your resources ran out and the electricity hasn't came you died\r\nPlay Again?";
+                    optionButton1.Text = "No";
+                    optionButton2.Text = "Yes";
+                    break;
+                case 16:
+                    outputLabel.Text = "According to the map you are on the way to escape. In the morning you realize there is no creature roaming around";
+                    Refresh();
+                    Thread.Sleep(3000);
+                    outputLabel.Text = "They are planning to reach a small hotel and according to the map it will take them 10 hours walk and 6 hours car";
+                    optionButton1.Text = "Walk";
+                    optionButton2.Text = "Find a Car nearby";
+                    break;
+                case 17:
+                    outputLabel.Text = "You ran out of resources and wasn't able to reach the hotel before evening you got hunted\r\nPlay Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 18:
+                    outputLabel.Text = "You found a car after 3 and half hours of walking you have to go 2 more";
+                    Refresh();
+                    Thread.Sleep(3000);
+                    outputLabel.Text = "You reached the hotel successfully and safely but as you were going to it you saw blood and dead bodies of people.";
+                    Refresh();
+                    Thread.Sleep(3000);
+                    outputLabel.Text = "You saw a creature trying to chase you  but lucky you ended up saving yourself in a room. The next morning you saw no creature outside so that made you wonder why the creature are not seen but they can in the night. There is a gas station nearby";
+                    optionButton1.Text = "It's fine, keep going";
+                    optionButton2.Text = "Fuel";
+                    break;
+                case 19:
+                    outputLabel.Text = "The car stopped mid way and no shelter was there you died\r\nPlay Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 20:
+                    outputLabel.Text = "You were close to escaping but suddenly your car got in engine problem ";
+                    optionButton1.Text = "Fix it";
+                    optionButton2.Text = "Walk 2 hours and escape";
+                    break;
+                case 21:
+                    outputLabel.Text = "You tried but too late\r\nPlay Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 22:
+                    optionButton3.Show();
+                    outputLabel.Text = "You chose walk for 2 hours but it turn evening and creature are getting outside";
+                    optionButton1.Text = "Run Separate";
+                    optionButton2.Text = "Pull out a gun and stay together";
+                    optionButton3.Text = "Hide";
+                    break;
+                case 23:
+                    optionButton3.Hide();
+                    outputLabel.Text = "You and your friend were hunted by different creature\r\nPlay Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 24:
+                    outputLabel.Text = "You were run out of bullets you died\r\nPlay Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 25:
+                    outputLabel.Text = "You survived the night and was successful escaping ";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    outputLabel.Text = "Game over \r\nPart 2 coming soon!\r\nPlay Again?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thanks for playing!";
+                    optionButton1.Text = "";
+                    optionButton2.Text = "";
+
+                    optionButton1.Hide();
+                    optionButton2.Hide();
+
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+            }
+        }
+
+        private void optionButton3_Click(object sender, EventArgs e)
+        {
+            if (page == 22)
+            {
+                page = 25;
+            }
         }
     }
 }
+
